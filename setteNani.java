5e19b26bccfc44e083fcb56b7e9bc2e94dc09fb7
GitScuola1234 <118480143+GitScuola1234@users.noreply.github.com>
2022-11-23 00:37:14 +0100

    Create setteNani.java

---
+++ setteNani.java
@@ -0,0 +1,9 @@
+class setteNani {
+  public static void main(String[] args) {
+    ContaINani1 thr1 = new ContaINani1();
+    thr1.start();
+    System.out.println(Thread.currentThread().getName());
+  }
+
+}
+
