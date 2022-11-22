4ac2cd924ac6df80e5f9c4998c580bfa88c935ff
GitScuola1234 <118480143+GitScuola1234@users.noreply.github.com>
2022-11-23 00:37:58 +0100

    Create ContaINani1.java

---
+++ ContaINani1.java
@@ -0,0 +1,9 @@
+public class ContaINani1 extends Thread {
+  public void run() {
+    setName("settenani");
+    System.out.println(Thread.currentThread().getName());
+    for (int i=0; i < 7; i++) {
+      System.out.println((i+1)+" ");
+    }
+  }
+}
